3fad1f98aa6da594ca8c05354a9638aacb35d832
bassyu <yrkdgus@gmail.com>
2023-04-13 15:05:44 +0900

    build: step1 배포 2

---
+++ precache-manifest.3998058e68bdb581f210af7aa339bf48.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a32c6f4f7316d647d545",
+    "url": "/react-lunch/static/js/main.a32c6f4f.chunk.js"
+  },
+  {
+    "revision": "860a1f007774113bc3e3",
+    "url": "/react-lunch/static/js/1.860a1f00.chunk.js"
+  },
+  {
+    "revision": "cb3244ec3c98e45966e7",
+    "url": "/react-lunch/static/js/runtime~main.cb3244ec.js"
+  },
+  {
+    "revision": "2375087cb9f2e6cfc5ad08d4a0d913ed",
+    "url": "/react-lunch/index.html"
+  }
+];
