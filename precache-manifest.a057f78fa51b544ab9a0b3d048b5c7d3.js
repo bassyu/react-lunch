e152958008e9c3b5bf959e321e1c42117ac0fd4c
bassyu <yrkdgus@gmail.com>
2023-04-15 15:19:27 +0900

    build: step2 빌드 2

---
+++ precache-manifest.a057f78fa51b544ab9a0b3d048b5c7d3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e673a00d4849c4f17a5",
+    "url": "/react-lunch/static/js/main.0e673a00.chunk.js"
+  },
+  {
+    "revision": "dc47824c6b064e62c20d",
+    "url": "/react-lunch/static/js/1.dc47824c.chunk.js"
+  },
+  {
+    "revision": "cb3244ec3c98e45966e7",
+    "url": "/react-lunch/static/js/runtime~main.cb3244ec.js"
+  },
+  {
+    "revision": "eeb5409b421d1b21b405a7366337a0d7",
+    "url": "/react-lunch/index.html"
+  }
+];
